339de2e89a59a5c216bcb20862550d5b5b79e6b5
agatakolohe <agatakolohe@gmail.com>
2021-01-20 13:05:30 -0800

    update readme
    Co-authored-by: Harrison Strand <harrisonstrand@gmail.com>

--- HeyYou/Startup.cs
+++ HeyYou/Startup.cs
@@ -27,8 +27,6 @@ namespace HeyYou
             .AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
-
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
